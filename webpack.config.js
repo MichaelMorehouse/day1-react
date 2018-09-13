5f6bf351617b88cb71bf2a34ec447655330daab0
Michael M <mimomiller@gmail.com>
2018-09-12 18:08:08 -0700

    add file and url loaders to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,26 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 8192
+                    }
+                  }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {}
+                  }
+                ]
             }
         ]
     }
